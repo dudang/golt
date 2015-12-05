df72cfc60f99a2b40ae0a6e3b43281957c69b627
Philibert Dugas <philibert.dugas@gmail.com>
2015-12-05 16:22:07 -0500

    Added first tests and make travis run them

---
+++ parser/parser_test.go
@@ -0,0 +1,17 @@
+package parser
+
+import (
+	"testing"
+)
+
+func TestParseInputFile(t *testing.T) {
+	_, err := ParseInputFile("../test/golt-test.json")
+	if err != nil {
+		t.Fail()
+	}
+
+	_, err = ParseInputFile("wrong_file.json")
+	if err == nil {
+		t.Fail()
+	}
+}
