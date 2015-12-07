8c403eed42bbc4b34827ef563829f8b25ca7eca1
Philibert Dugas <philibert.dugas@gmail.com>
2015-12-06 21:47:22 -0500

    Added timeout validation in the HTTP Client

--- runner/runner.go
+++ runner/runner.go
@@ -11,10 +11,10 @@ import (
 	"github.com/dudang/golt/logger"
 )
 
+const dateFormat = "2006-01-02 15:04:05"
+
 var internalWaitGroup sync.WaitGroup
 var stageWaitGroup sync.WaitGroup
-
-var httpClient = &http.Client{}
 
 func ExecuteGoltTest(goltTest parser.Golts) {
 	m := generateGoltMap(goltTest)
@@ -35,39 +35,65 @@ func ExecuteGoltTest(goltTest parser.Golts) {
 func executeStage(stage []parser.GoltItem) {
 	stageWaitGroup.Add(len(stage))
 	for _, item := range stage{
-		go executeItem(item)
+		httpClient := generateHttpClient(item)
+		go executeItem(item, httpClient)
 	}
 	stageWaitGroup.Wait()
 }
 
-func executeItem(item parser.GoltItem) {
+func executeItem(item parser.GoltItem, httpClient *http.Client) {
 	internalWaitGroup.Add(item.Threads)
+
 	for i:= 0; i < item.Threads; i++ {
-		go executeHttpRequest(item)
+		go executeHttpRequest(item, httpClient)
 	}
+
 	internalWaitGroup.Wait()
 	stageWaitGroup.Done()
 }
 
-func executeHttpRequest(item parser.GoltItem) {
+func generateHttpClient(item parser.GoltItem) *http.Client {
+	var httpClient *http.Client
+	if item.Assert.Timeout > 0 {
+		httpClient = &http.Client{
+			Timeout: time.Duration(time.Millisecond * time.Duration(item.Assert.Timeout)),
+		}
+	} else {
+		httpClient = &http.Client{}
+	}
+	return httpClient
+}
+
+func executeHttpRequest(item parser.GoltItem, httpClient *http.Client) {
 	for i := 1; i <= item.Repetitions; i++ {
 		payload := []byte(item.Payload)
 		req, err := http.NewRequest(item.Method, item.URL, bytes.NewBuffer(payload))
 
 		resp, err := httpClient.Do(req)
+
 		var msg string
 		if err != nil {
-			msg = fmt.Sprintf("%v\n", err)
+			msg = fmt.Sprintf("[%s] Stage: %d Repetitions: %d Message: %v\n",
+				time.Now().Format(dateFormat),
+				item.Stage,
+				i,
+				err)
+		} else {
+			defer resp.Body.Close()
+			isSuccess := isCallSuccessful(item.Assert, resp)
+			msg = fmt.Sprintf("[%s] Stage: %d Repetitions: %d  Status Code: %d Success: %t\n",
+				time.Now().Format(dateFormat),
+				item.Stage,
+				i,
+				resp.StatusCode,
+				isSuccess)
 		}
-		defer resp.Body.Close()
-
-		msg = fmt.Sprintf("[%s] Stage: %d Repetitions: %d  Status Code: %d Success: %t\n",
-			time.Now().Format("2006-01-02 15:04:05"),
-			item.Stage,
-			i,
-			resp.StatusCode,
-			resp.StatusCode == item.Assert.Status)
 		logger.Log([]byte(msg))
 	}
 	internalWaitGroup.Done()
+}
+
+func isCallSuccessful(assert parser.GoltAssert, response *http.Response) bool{
+	// TODO: Finish this method to validate more than just response code
+	return assert.Status == response.StatusCode
 }
