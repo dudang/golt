3a59f18a6845319ecfe6da0b2c6f12f2c4c5f33f
Philibert Dugas <philibert.dugas@gmail.com>
2015-12-18 21:46:24 -0500

    Added throughput counter every 5 seconds

--- runner/watcher.go
+++ runner/watcher.go
@@ -2,6 +2,7 @@ package runner
 
 import (
 	"time"
+	"fmt"
 )
 
 var requestSent int64
@@ -10,11 +11,18 @@ var start time.Time
 // TODO: Calculate throughput during the test, not just at the end
 func Watch(channel chan []byte) {
 	requestSent = 0
+	timeCounter := 5.0
 	start = time.Now()
+	var delta time.Duration
 	for {
 		event := <- channel
 		if event != nil {
 			requestSent++
+		}
+		delta = time.Since(start)
+		if delta.Seconds() > timeCounter {
+			fmt.Printf("Throughput in the last 5 seconds: %f R/S\n", (float64(requestSent)/delta.Seconds()))
+			timeCounter += 5.0
 		}
 	}
 }
