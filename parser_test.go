c009e0dbab515c6d2d2b95910b0dccdb9dc43b15
Philibert Dugas <philibert.dugas@gmail.com>
2015-12-23 18:13:38 -0500

    Added a few tests to the parser method

--- parser_test.go
+++ parser_test.go
@@ -10,7 +10,17 @@ func TestParseInputFile(t *testing.T) {
 		t.Fail()
 	}
 
+	_, err = ParseInputFile("test/golt-test.yaml")
+	if err != nil {
+		t.Fail()
+	}
+
 	_, err = ParseInputFile("wrong_file.json")
+	if err == nil {
+		t.Fail()
+	}
+
+	_, err = ParseInputFile("test/golt-test.txt")
 	if err == nil {
 		t.Fail()
 	}
